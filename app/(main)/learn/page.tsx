0373ccc254654b9f841377567571ebf4263a3318
benjii66 <benjamin51.simon@gmail.com>
2024-04-09 21:14:29 +0200

    course progression backend is ok !

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -3,24 +3,35 @@ import { StickyWrapper } from '@/components/stickwrapper'
 import React from 'react'
 import { Header } from './header'
 import { UserProgress } from '@/components/user-progress'
-import { getUnits, getUserProgress } from '@/db/queries'
+import { getCourseProgress, getLessonPercentage, getUnits, getUserProgress } from '@/db/queries'
 import { redirect } from 'next/navigation'
 import { Unit } from './unit'
+import { lessons, units as unitsSchema } from '@/db/schema'
 
 const LearnPage = async () => {
   const userProgressData = getUserProgress();
   const unitsData = getUnits();
+  const courseProgressData = getCourseProgress();
+  const lessonPercentageData = getLessonPercentage();
 
   const [
     userProgress,
-    units
+    units,
+    courseProgress,
+    lessonPercentage
   ] = await Promise.all([
     userProgressData,
-    unitsData
+    unitsData,
+    courseProgressData,
+    lessonPercentageData
   ]);
  
  if(!userProgress || !userProgress.activeCourseId)
   redirect('/courses')
+
+ if(!courseProgress)
+    redirect('/courses')
+
 
   return (
     <div className ="flex flex-row-reverse gap-[48px] px-6">
@@ -42,8 +53,8 @@ const LearnPage = async () => {
                   description={unit.description}
                   title={unit.title}
                   lessons={unit.lessons}
-                  activeLesson={undefined}
-                  activeLessonPercentage={0}
+                  activeLesson={courseProgress.activeLesson}
+                  activeLessonPercentage={lessonPercentage}
                 />
               </div>
               ))}
